e37c394907e897c1e2a47fdce5bd867811267f0d
danielefongo <danielefongo@gmail.com>
2020-12-23 11:55:42 +0100

    big bang

---
+++ index.js
@@ -0,0 +1,120 @@
+import io from "socket.io-client";
+
+class HalloClient {
+  constructor(iceServers) {
+    this.socket = io()
+    this.peers = {}
+    this.room = undefined
+    this.localStream = undefined
+    this.iceServers = iceServers
+  }
+
+  join(room, constraints, callbacks) {
+    if (!room) throw new Error("Invalid room")
+
+    this.room = room
+    this.constraints = constraints
+    this.callbacks = callbacks
+    this.prepareSocket()
+    this.socket.emit('hallo_join', room)
+  }
+
+  leave() {
+    this.socket.emit('hallo_left', this.room)
+  }
+
+  prepareSocket() {
+    this.socket.on('hallo_created', async () => {
+      await this.loadStream()
+    })
+
+    this.socket.on('hallo_joined', async () => {
+      await this.loadStream()
+      this.socket.emit('hallo_new_peer', this.room)
+    })
+
+    this.socket.on('hallo_left', async (peerId) => {
+      this.callbacks.removeRemoteStream(this.peers[peerId].stream)
+      this.peers[peerId].close()
+    })
+
+    this.socket.on('hallo_new_peer', async (peerId) => {
+      this.newPeer(peerId)
+      await this.createOffer(peerId)
+    })
+
+    this.socket.on('hallo_offer', async (peerId, event) => {
+      this.newPeer(peerId)
+      this.addRemote(peerId, event)
+      await this.createAnswer(peerId)
+    })
+
+    this.socket.on('hallo_answer', (peerId, event) => {
+      this.addRemote(peerId, event)
+    })
+
+    this.socket.on('hallo_candidate', (peerId, event) => {
+      var candidate = new RTCIceCandidate(event.candidate)
+      this.peers[peerId].addIceCandidate(candidate)
+    })
+  }
+
+  async loadStream() {
+    try {
+      this.localStream = await navigator.mediaDevices.getUserMedia(this.constraints)
+      this.callbacks.addLocalStream(this.localStream)
+    } catch (error) {
+      console.error('Could not get user media', error)
+    }
+  }
+
+  newPeer(peerId) {
+    this.peers[peerId] = new RTCPeerConnection(this.iceServers)
+    this.addLocalTracks(this.peers[peerId])
+    this.peers[peerId].ontrack = (e) => {
+      this.peers[peerId].stream = e.streams[0]
+      this.callbacks.addRemoteStream(e.streams[0])
+    }
+    this.peers[peerId].onicecandidate = (e) => this.sendIceCandidate(e, peerId)
+  }
+
+  addRemote(peerId, event) {
+    this.peers[peerId].setRemoteDescription(new RTCSessionDescription(event))
+  }
+
+  async createOffer(peerId) {
+    try {
+      const sdp = await this.peers[peerId].createOffer()
+      this.peers[peerId].setLocalDescription(sdp)
+
+      this.socket.emit('hallo_offer', {sdp, peerId})
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async createAnswer(peerId) {
+    try {
+      const sdp = await this.peers[peerId].createAnswer()
+      this.peers[peerId].setLocalDescription(sdp)
+
+      this.socket.emit('hallo_answer', {sdp, peerId})
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  sendIceCandidate(event, peerId) {
+    if (event.candidate) {
+      this.socket.emit('hallo_candidate', {candidate: event.candidate, peerId})
+    }
+  }
+
+  addLocalTracks(rtcPeerConnection) {
+    this.localStream.getTracks().forEach((track) => {
+      rtcPeerConnection.addTrack(track, this.localStream)
+    })
+  }
+}
+
+export default HalloClient
---
+++ readme.md
@@ -0,0 +1,29 @@
+# hallo client
+
+Simple webrtc client for [hallo-server](https://www.npmjs.com/package/hallo-server).
+
+## usage
+
+```javascript
+const mediaConstraints = {
+  audio: true,
+  video: { width: 1280, height: 720 },
+}
+
+const iceServers = {
+  iceServers: [{ urls: 'stun:stun.l.google.com:19302'}]
+}
+const hallo = new HalloClient(iceServers)
+
+hallo.join("a_room", mediaConstraints, {
+  addLocalStream: (stream) => doStuff(),
+  addRemoteStream: (stream) => doStuff(),
+  removeRemoteStream: (stream) => doStuff()
+})
+
+/*
+...
+*/
+
+hallo.leave()
+```
